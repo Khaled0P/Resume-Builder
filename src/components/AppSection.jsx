d2140996cee861e4111aa4118a3bc3cb866cc380
Khaled AbdRahem <aa.khaled11@gmail.com>
2023-08-30 19:54:34 +0300

    add form drop down code & animation

--- src/components/AppSection.jsx
+++ src/components/AppSection.jsx
@@ -1,13 +1,33 @@
 import AppForm from './form';
 import '../styles/AppSection.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
-export default function AppSection({ fieldName, icon, list, setData, data }) {
+export default function AppSection({
+  fieldName,
+  icon,
+  list,
+  setData,
+  data,
+  onClick,
+  className,
+}) {
   return (
-    <div className="container">
-      <h2>
-        {icon} {fieldName}
-      </h2>
-      <AppForm list={list} setData={setData} data={data} />
+    <div className={className}>
+      <div className="header">
+        <h2>
+          {icon} {fieldName}
+          <button onClick={onClick} className="dropDownBtn">
+            <FontAwesomeIcon
+              icon="fa-solid fa-angle-up"
+              rotation={180}
+              style={{ color: '#0d0c0c' }}
+            />
+          </button>
+        </h2>
+      </div>
+      <div className="dropDownForm">
+        <AppForm list={list} setData={setData} data={data} />
+      </div>
     </div>
   );
 }
--- src/components/Customize.jsx
+++ src/components/Customize.jsx
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import AppSection from './AppSection';
 import '../styles/Customize.css';
 import DefaultTemplateControl from './defaultTemplateControl';
@@ -16,6 +17,11 @@ export default function Customize({
   education,
   experience,
 }) {
+  const [dropDown, setDropDown] = useState(1);
+
+  function handleClick(value) {
+    dropDown === value ? setDropDown(0) : setDropDown(value);
+  }
   return (
     <div className="customize">
       <DefaultTemplateControl control={control} />
@@ -24,18 +30,24 @@ export default function Customize({
         list={personalFieldInfo}
         setData={setData}
         data={data}
+        onClick={() => handleClick(1)}
+        className={`container ${dropDown === 1 ? 'dropDown' : ''}`}
       />
       <AppSection
         fieldName="Education"
         list={educationFieldInfo}
         setData={setEducation}
         data={education}
+        onClick={() => handleClick(2)}
+        className={`container ${dropDown === 2 ? 'dropDown' : ''}`}
       />
       <AppSection
         fieldName="Experience"
         list={experienceFieldInfo}
         setData={setExperience}
         data={experience}
+        onClick={() => handleClick(3)}
+        className={`container ${dropDown === 3 ? 'dropDown' : ''}`}
       />
     </div>
   );
